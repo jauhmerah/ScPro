68382b8982a332ac00dd4965ab12db0d73975ad5
jauhmerah <jauhmerah@gmail.com>
2016-08-16 00:11:12 +0800

    stamp 16082016.1209

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = "nasty_v2/dashboard";
+$route['default_controller'] = "order";
 $route['404_override'] = '';
 
 
--- application/views/dashboard/history.php
+++ application/views/dashboard/history.php
@@ -1,136 +1,136 @@
 <div class="row">
 	<div class="col-md-12">
 		<div class="table-responsive">
-					<table class="table table-hover table-striped table-bordered">
-						<thead>
-							<tr style="background-color: #F5F5F5">
-								<th>#</th>
-								<th>Client Name</th>
-								<th>Contact No</th>
-								<th>Country</th>
-								<th>Deposit</th>							
-								<th>Send Date</th>
-								<th>Action</th>
-							</tr>
-						</thead>
-						<tbody>					
-							<?php
-							if (sizeof($arr) == 0) { ?>
-								<tr>
-									<td colspan = "7">
-										<div align = 'center'>
-											No History...
-										</div>
-									</td>
-								</tr>
-							<?php									
-							}else{							
-							$n = 0; 
-							foreach ($arr as $key) { 
-								$n++;
-							?>
-							<tr >
-								<td rowspan = "2"><?= $n; ?></td>
-								<td><?= $key->cl_name; ?></td>
-								<td><?= $key->cl_tel; ?></td>
-								<td><?= $key->cl_country; ?></td>
-								<td><?= $key->or_deposit; ?></td>
-								<td><?= $key->or_sendDate; ?></td>								
-								<td>
-								<div class="btn-group">
-									<a href="#" class="listO" id="l<?= $n; ?>" title="View Detail"><button type="button" class="btn btn-info"><i class="fa fa-eye"></i></button></a>									
-									<!--<a href="" title="Edit"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;-->	
+			<table class="table table-hover table-striped table-bordered">
+				<thead>
+					<tr style="background-color: #F5F5F5">
+						<th>#</th>
+						<th>Client Name</th>
+						<th>Contact No</th>
+						<th>Country</th>
+						<th>Deposit</th>							
+						<th>Send Date</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>					
+					<?php
+					if (sizeof($arr) == 0) { ?>
+						<tr>
+							<td colspan = "7">
+								<div align = 'center'>
+									No History...
 								</div>
-								</td>
-								
-							</tr>
+							</td>
+						</tr>
+					<?php									
+					}else{							
+					$n = 0; 
+					foreach ($arr as $key) { 
+						$n++;
+					?>
+					<tr >
+						<td rowspan = "2"><?= $n; ?></td>
+						<td><?= $key->cl_name; ?></td>
+						<td><?= $key->cl_tel; ?></td>
+						<td><?= $key->cl_country; ?></td>
+						<td><?= $key->or_deposit; ?></td>
+						<td><?= $key->or_sendDate; ?></td>								
+						<td>
+						<div class="btn-group">
+							<a href="#" class="listO" id="l<?= $n; ?>" title="View Detail"><button type="button" class="btn btn-info"><i class="fa fa-eye"></i></button></a>									
+							<!--<a href="" title="Edit"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;-->	
+						</div>
+						</td>
+						
+					</tr>
+					<?php 
+						$size = sizeof($key->item);
+						$n2 = 0 ;
+						foreach ($key->item as $item) {
+							if ($item->pr_id == '3') {
+								$n2++;										
+							}
+						}
+						$peratus = ($n2/$size)*100;
+					?>
+					<tr>
+						<td colspan = "5">
+						<div class="progress">
+		                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?= $peratus; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?= $peratus; ?>%;"><span class="sr-only"><?= $peratus; ?>% Complete</span></div>
+		                </div>								
+						</td>
+						<td>
 							<?php 
-								$size = sizeof($key->item);
-								$n2 = 0 ;
-								foreach ($key->item as $item) {
-									if ($item->pr_id == '3') {
-										$n2++;										
-									}
-								}
-								$peratus = ($n2/$size)*100;
-							?>
-							<tr>
-								<td colspan = "5">
-								<div class="progress">
-				                    <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="<?= $peratus; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?= $peratus; ?>%;"><span class="sr-only"><?= $peratus; ?>% Complete</span></div>
-				                </div>								
-								</td>
-								<td>
-									<?php 
-									switch ($key->pr_id) {
-										case '1':
-											?>
-											<span class="label label-warning">New Order</span>
-											<?php
-											break;
-										case '2':
-											?>
-											<span class="label label-info">In Progress</span>
-											<?php
-											break;
-										case '3':
-											?>
-											<span class="label label-success">Order Completed</span>
-											<?php
-											break;
-									}
+							switch ($key->pr_id) {
+								case '1':
 									?>
-								</td>
-							</tr>					
-							<tr class = "l<?= $n; ?> detail" style="display: none; ">								
-								<td colspan="7">								
-									<div class="row col-md-12">
-									<?php 
-										foreach ($key->item as $item) {
-									?>
-										<div class="well col-md-4" <?php if($item->pr_id == 3){ ?> style = "background-color : #73C10B;"<?php } ?>>										
-										  	<div class="media">
-												<a class="media-left" href="#">
-												  <img class="media-object" src="<?php echo base_url('assets/uploads/item').'/'.$item->ty_icon; ?> " alt="Generic placeholder image">
-												  <span class="label media-object" style = "background-color:<?= $item->ni_color; ?>;color : black;"><?= $item->ni_mg; ?> Mg</span>'
-												</a>
-												<div class="media-body">												
-													<h3 class="media-heading"><?= $item->ty_desc; ?></h3>
-											  		<span class="pull-left">Qty :</span><span class="pull-right"><?= $item->it_qty; ?></span></br>
-											  		<span class="pull-left">Promo :</span><span class="pull-right">+<?= $item->it_promo; ?></span>
-												</div>	
-											</div>																				
-										</div>
+									<span class="label label-warning">New Order</span>
 									<?php
-										}
+									break;
+								case '2':
 									?>
-																			
-									</div>									
-									<div class="clearfix">
-										&nbsp;
+									<span class="label label-info">In Progress</span>
+									<?php
+									break;
+								case '3':
+									?>
+									<span class="label label-success">Order Completed</span>
+									<?php
+									break;
+							}
+							?>
+						</td>
+					</tr>					
+					<tr class = "l<?= $n; ?> detail" style="display: none; ">								
+						<td colspan="7">								
+							<div class="row col-md-12">
+							<?php 
+								foreach ($key->item as $item) {
+							?>
+								<div class="well col-md-4" <?php if($item->pr_id == 3){ ?> style = "background-color : #73C10B;"<?php } ?>>										
+								  	<div class="media">
+										<a class="media-left" href="#">
+										  <img class="media-object" src="<?php echo base_url('assets/uploads/item').'/'.$item->ty_icon; ?> " alt="Generic placeholder image">
+										  <span class="label media-object" style = "background-color:<?= $item->ni_color; ?>;color : black;"><?= $item->ni_mg; ?> Mg</span>'
+										</a>
+										<div class="media-body">												
+											<h3 class="media-heading"><?= $item->ty_desc; ?></h3>
+									  		<span class="pull-left">Qty :</span><span class="pull-right"><?= $item->it_qty; ?></span></br>
+									  		<span class="pull-left">Promo :</span><span class="pull-right">+<?= $item->it_promo; ?></span>
+										</div>	
+									</div>																				
+								</div>
+							<?php
+								}
+							?>
+																	
+							</div>									
+							<div class="clearfix">
+								&nbsp;
+							</div>
+							<div class="row">
+							<div class = "col-md-12">
+								<div class="panel panel-info">
+									<div class="panel-heading">
+										Note
 									</div>
-									<div class="row">
-									<div class = "col-md-12">
-										<div class="panel panel-info">
-											<div class="panel-heading">
-												Note
-											</div>
-											<div class="panel-body">
-												<?= $key->or_note; ?>
-											</div>
-										</div>
+									<div class="panel-body">
+										<?= $key->or_note; ?>
 									</div>
-									</div>								
-								</td>
-							</tr>
-						<?php		
-							}}
-						?>	
+								</div>
+							</div>
+							</div>								
+						</td>
+					</tr>
+				<?php		
+					}}
+				?>	
 
-						<!-- End of table -->	
-						</tbody>
-					</table>
-				</div>
+				<!-- End of table -->	
+				</tbody>
+			</table>
+		</div>
 	</div>
 </div>
 <script>
