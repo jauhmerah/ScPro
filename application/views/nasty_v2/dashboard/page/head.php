90d0085e3be4f52783ffcf896b465f5247166215
jauhmerah <jauhmerah@gmail.com>
2017-01-02 02:59:00 +0800

    stamp 02012017.0258 Rechange deleted coding in head

--- application/views/nasty_v2/dashboard/page/head.php
+++ application/views/nasty_v2/dashboard/page/head.php
@@ -42,7 +42,10 @@ License: You must have a valid license purchased only from themeforest(the above
         <link href="<?= base_url(); ?>asset2/layouts/layout2/css/themes/blue.min.css" rel="stylesheet" type="text/css" id="style_color" />
         <link href="<?= base_url(); ?>asset2/layouts/layout2/css/custom.min.css" rel="stylesheet" type="text/css" />
         
+        <!-- BEGIN PAGE LEVEL PLUGINS -->
+        <link href="<?= base_url(); ?>asset2/global/plugins/select2/css/select2.min.css" rel="stylesheet" type="text/css">
+        <link href="<?= base_url(); ?>asset2/global/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css">
+        <!-- END THEME LAYOUT STYLES -->
         <link rel="icon" href="<?= base_url(); ?>assets/cover/favicon2.png"> 
         <script src="<?= base_url(); ?>asset/js/jquery-1.11.1.js"></script>        <script src="<?= base_url(); ?>asset/js/bootstrap.min.js"></script>
-       
     </head><!-- END HEAD -->
