0b0a33fbc319b007f8169fbc6e1ad8558e953fea
jauhmerah <jauhmerah@gmail.com>
2017-04-19 23:28:24 +0800

    stamp 19042017.2328

--- application/views/reseller/addorder.php
+++ application/views/reseller/addorder.php
@@ -3,27 +3,173 @@
 		<div class="portlet light">
             <div class="portlet-title">
                 <div class="caption">
-                    <i class="icon-paper-plane font-yellow-casablanca"></i>
-                    <span class="caption-subject bold font-yellow-casablanca uppercase"> Form Input </span>
+                    <i class="fa fa-cubes font-yellow-casablanca"></i>
+                    <span class="caption-subject bold font-yellow-casablanca uppercase"> Product List </span>
                     <span class="caption-helper">more samples...</span>
                 </div>
                 <div class="inputs">
-                    <div class="portlet-input input-inline input-medium">
+                    <!--<div class="portlet-input input-inline input-medium">
                         <div class="input-group">
                             <input type="text" class="form-control input-circle-left" placeholder="search...">
                             <span class="input-group-btn">
                                 <button class="btn btn-circle-right btn-default" type="submit">Go!</button>
                             </span>
                         </div>
-                    </div>
+                    </div>-->
                 </div>
-            </div>
-            <div class="portlet-body">
-                <h4>Heading text goes here...</h4>
-                <p> Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
-                    lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
-                    fermentum. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. </p>
-            </div>
+            </div>            
+        </div>
+        <div class="row">
+	    	<div class="col-md-6">
+	            <div class="portlet light portlet-fit ">
+	                <div class="portlet-title">
+	                    <div class="caption">
+	                        <i class=" icon-layers font-green"></i>
+	                        <span class="caption-subject font-green bold uppercase">Overlay 4 - Icons</span>	                        
+	                    </div>
+	                </div>
+	                <div class="portlet-body">
+	                    <div class="mt-element-overlay">
+	                        <div class="row">
+	                            <div class="col-md-12">
+	                                <div class="mt-overlay-4 mt-overlay-4-icons">
+	                                    <img src="http://keenthemes.com/preview/metronic/theme/assets/pages/img/page_general_search/06.jpg">
+	                                    <div class="mt-overlay">
+	                                        <h2>Overlay Title</h2>
+	                                        <ul class="mt-info">
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="fa fa-fw fa-tint"> 0 Mg</i>
+	                                                </a>
+	                                            </li>
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="fa fa-fw fa-tint"> 3 Mg</i>
+	                                                </a>
+	                                            </li>
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="fa fa-fw fa-tint"> 6 Mg</i>
+	                                                </a>
+	                                            </li>
+	                                        </ul>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="col-md-6">
+	            <div class="portlet light portlet-fit ">
+	                <div class="portlet-title">
+	                    <div class="caption">
+	                        <i class=" icon-layers font-green"></i>
+	                        <span class="caption-subject font-green bold uppercase">Overlay 4 - Icons</span>	                        
+	                    </div>
+	                </div>
+	                <div class="portlet-body">
+	                    <div class="mt-element-overlay">
+	                        <div class="row">
+	                            <div class="col-md-12">
+	                                <div class="mt-overlay-4 mt-overlay-4-icons">
+	                                    <img src="http://keenthemes.com/preview/metronic/theme/assets/pages/img/page_general_search/06.jpg">
+	                                    <div class="mt-overlay">
+	                                        <h2>Overlay Title</h2>
+	                                        <ul class="mt-info">
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="icon-magnifier"></i>
+	                                                </a>
+	                                            </li>
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="icon-link"></i>
+	                                                </a>
+	                                            </li>
+	                                        </ul>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="col-md-6">
+	            <div class="portlet light portlet-fit ">
+	                <div class="portlet-title">
+	                    <div class="caption">
+	                        <i class=" icon-layers font-green"></i>
+	                        <span class="caption-subject font-green bold uppercase">Overlay 4 - Icons</span>	                        
+	                    </div>
+	                </div>
+	                <div class="portlet-body">
+	                    <div class="mt-element-overlay">
+	                        <div class="row">
+	                            <div class="col-md-12">
+	                                <div class="mt-overlay-4 mt-overlay-4-icons">
+	                                    <img src="http://keenthemes.com/preview/metronic/theme/assets/pages/img/page_general_search/06.jpg">
+	                                    <div class="mt-overlay">
+	                                        <h2>Overlay Title</h2>
+	                                        <ul class="mt-info">
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="icon-magnifier"></i>
+	                                                </a>
+	                                            </li>
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="icon-link"></i>
+	                                                </a>
+	                                            </li>
+	                                        </ul>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	        <div class="col-md-6">
+	            <div class="portlet light portlet-fit ">
+	                <div class="portlet-title">
+	                    <div class="caption">
+	                        <i class=" icon-layers font-green"></i>
+	                        <span class="caption-subject font-green bold uppercase">Overlay 4 - Icons</span>	                        
+	                    </div>
+	                </div>
+	                <div class="portlet-body">
+	                    <div class="mt-element-overlay">
+	                        <div class="row">
+	                            <div class="col-md-12">
+	                                <div class="mt-overlay-4 mt-overlay-4-icons">
+	                                    <img src="http://keenthemes.com/preview/metronic/theme/assets/pages/img/page_general_search/06.jpg">
+	                                    <div class="mt-overlay">
+	                                        <h2>Overlay Title</h2>
+	                                        <ul class="mt-info">
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="icon-magnifier"></i>
+	                                                </a>
+	                                            </li>
+	                                            <li>
+	                                                <a class="btn default btn-outline" href="javascript:;">
+	                                                    <i class="icon-link"></i>
+	                                                </a>
+	                                            </li>
+	                                        </ul>
+	                                    </div>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	        
         </div>		
 	</div>
 	<div class="col-md-6">
