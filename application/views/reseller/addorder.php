bb9485f297e5988e27a951349a0cd9f32df3edc4
jauhmerah <jauhmerah@gmail.com>
2017-05-23 22:40:44 +0800

    stamp 23052017.2240

--- application/views/reseller/addorder.php
+++ application/views/reseller/addorder.php
@@ -1,3 +1,6 @@
+<div>
+    
+</div>
 <div class="row">
 	<div class="col-md-12">        
 		<div class="portlet light">
@@ -11,99 +14,124 @@
         <div class="portlet-body">
             <div class="row">
                 <div class="col-xs-12">
-                    <div class="col-xs-4">
-                        <a class="dashboard-stat dashboard-stat-v2" style=" border: 2px solid #337ab7;" href="javascript:productList(1);">
-                            <div class="visual">
-                                <i class="fa fa-cube"></i>
+                    <div class="col-xs-8">
+                    <div class="row">
+                        <div class="progress progress-striped active" style="height: 40px;">
+                            <div class="progress-bar" role="progressbar" style="width: 19%">
+                                <span class="sr-only"> 40% Complete (success) </span>
                             </div>
-                            <div class="details">
-                                <div class="number">
-                                    <span data-counter="counterup" data-value="<?= $itemNum ?>">0</span>
-                                </div>
-                                <div class="desc">À la carte</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-xs-4">
-                        <a class="dashboard-stat dashboard-stat-v2" style=" border: 2px solid #337ab7;" href="javascript:productList(2);">
-                            <div class="visual">
-                                <i class="fa fa-cubes"></i>
-                            </div>
-                            <div class="details">
-                                <div class="number">
-                                    <span data-counter="counterup" data-value="<?= $packNum; ?>">0</span>
-                                </div>
-                                <div class="desc">Package</div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col-xs-4">
-                        <a class="dashboard-stat dashboard-stat-v2" style=" border: 2px solid #337ab7;" href="javascript:;">
-                            <div class="visual">
-                                <i class="fa fa-briefcase"></i>
-                            </div>
-                            <div class="details">
-                                <div class="number">
-                                    <span data-counter="counterup" data-value="0">0</span>
-                                </div>
-                                <div class="desc">Merchandise</div>
-                            </div>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="clearfix">
-                <br>
-            </div>
-            <div class="row">
-                    <div class="col-xs-6">
-                        <h2><strong><span id="listTitle"><i class="fa fa-cube"></i> À la carte</span></strong></h2>
-                    </div>
-                <div class="col-xs-6 hidden">
-                    <div class="form-group">
-                        <label for="input" class="col-sm-2 control-label">Search :</label>
-                        <div class="col-sm-10">
-                            <input type="text" name="" id="input" class="form-control input-circle input-sm" value="" placeholder="# Item Code">
                         </div>
                     </div>
-                    <div class="clearfix">
-                        &nbsp;
+                    <div class="row">
+                        <div class="progress">
+                            <div class="progress-bar progress-bar-danger" style="width: 19%">
+                                <span class="sr-only"> Deny </span>
+                            </div>
+                            <div class="progress-bar purple-plum" style="width: 22%">
+                                <span class="sr-only"> 20 - 40 pcs </span>
+                            </div>
+                            <div class="progress-bar progress-bar-warning" style="width: 19%">
+                                <span class="sr-only"> 41 - 60 pcs</span>
+                            </div>
+                            <div class="progress-bar progress-bar-success" style="width: 40%">
+                                <span class="sr-only"> 61 - 100 pcs </span>
+                            </div>
+                        </div>
                     </div>
-                     <div class="form-group ">                 
-                        <label for="input" class="col-sm-2 control-label">Series :</label>
-                        <div class="col-sm-10">                            
-                            <select name="siri" id="siri" class="form-control input-circle input-sm">
-                                <option value="-1">-- Select All --</option>
-                                <?php 
-                                    foreach ($cat as $key) {
-                                        ?>
-                                        <option value="<?= $this->my_func->en($key->ca_id); ?>"><?= $key->ca_desc; ?></option>
-                                        <?php
-                                    }
-                                ?>
-                            </select>
-                        </div>                        
+                    <div class="row">
+                        <span class="label label-danger">Deny</span> <= 19 pcs <span class="label bg-purple-plum bg-font-purple-plum">RM 17.50</span> : 20-40 pcs <span class="label label-warning">RM 17.00</span> : 41-60 pcs <span class="label label-success">RM 16.50</span> : 61-100 pcs
+                    </div>                    
                     </div>
-                </div>
-            </div>
-            <div class="clearfix">
-                &nbsp;
-            </div>
-            <div class="row mt-element-card mt-element-overlay Page" id="pro">
-                <div align="center">
-                    <h1><i class="fa fa-spinner fa-spin"></i></h1><br>
-                    <h3><strong>Brewing In Progress</strong></h3>
-                </div>
-            </div>
-            <div style="display: none;" id="loading1">
-                <div align="center">
-                    <h1><i class="fa fa-spinner fa-spin"></i></h1><br>
-                    <h3><strong>Brewing In Progress</strong></h3>
-                </div>
-            </div>
+                    <div class="col-xs-4">
+                        <a class="dashboard-stat dashboard-stat-v2" style=" border: 2px solid #337ab7;">
+                            <div class="visual">
+                                <i>RM</i>
+                            </div>
+                            <div class="details">
+                                <div class="number">
+                                    <span data-counter="counterup" data-value="17.50">0</span>
+                                </div>
+                                <div class="desc">Price Rate</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>                
+            </div>      
         </div>
         </div>
 	</div>
+</div>
+<div class="row">
+    <dir class="col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="input" class="col-sm-2 control-label">Series :</label>
+                            <div class="col-sm-10">
+                                <select name="" id="input" class="form-control input-circle">
+                                    <option value="">-- Select One --</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix">
+                    <br>
+                    </div>
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="input" class="col-sm-2 control-label">Flavor :</label>
+                            <div class="col-sm-10">
+                                <select name="" id="input" class="form-control input-circle">
+                                    <option value="">-- Select One --</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="form-group">
+                            <label for="input" class="col-sm-2 control-label">Nico :</label>
+                            <div class="col-sm-10">
+                                <label>6 mg</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="clearfix">
+                        <br>
+                    </div>
+                    <div class="row">
+                        <span class="pull-left" style="padding-top: 15px;"><i class="fa fa-spinner fa-spin"></i> <i class="fa fa-flask"></i> Brewing In Progress</span><span class="pull-right"><button type="button" class="btn btn-primary btn-circle"><i class="fa fa-plus"></i> Add</button></span>
+                    </div>
+                </div>
+                <div class="clearfix">
+                    <br>
+                    <hr>
+                </div>
+                <div class="clearfix">
+                    <br>
+                </div>
+                <table class="table table-condensed table-hover">
+                    <thead>
+                        <tr>
+                            <th>Flavor Detail</th>
+                            <th>Quantity</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Bad Blood</td>
+                            <td><input type="number" name="qty" id="inputQty" class="form-control input-circle" min="1" max="100" step="1" required="required" title="Quantity"></td>
+                            <td><span class="pull-right"><button type="button" class="btn btn-danger btn-circle "><i class="fa fa-trash"></i></button></span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </dir>    
 </div>
 
 <!-- invoice end -->
